5f5cb6fcf0e990ded00241948160797634a5c272
kombeizD <kombeiz.daniel@gmail.com>
2025-06-06 13:32:58 +0200

    made buttons working, improved visuals

--- src/ApikeyManager.py
+++ src/ApikeyManager.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, jsonify
+import json
+
+from flask import Flask, render_template, jsonify, request
 from flaskwebgui import FlaskUI
 import requests
 
@@ -22,21 +24,25 @@ def get_api_keys():
 
 @app.route('/api-keys', methods=['POST'])
 def add_api_key():
-    head = { "Content-Type": "application/xml"}
+    data = request.get_json()
+    head = {"Authorization": "Bearer xxxAdmin1234", "Content-Type": "application/xml"}
     url = "http://localhost:8080/api-keys"
-    xml_data=f"<ApiKeyCred><apiKey>{apiKeyDTO.get_api_key()}</apiKey><clientDn>{apiKeyDTO.get_clientDN()}</clientDn></ApiKeyCred>"
-    response = requests.post(url, headers=head, data=xml_data, timeout=5)
+    response = requests.post(url, headers=head, data=data, timeout=5)
     response.raise_for_status()
 
 @app.route('/api-keys-activate', methods=['POST'])
 def activate_api_key():
-    url = f"http://localhost:8080/api-keys/{apiKeyDTO.get_api_key()}/activate"
+    data = json.dumps(request.get_json())
+    data_clean = data.replace('"',"")
+    url = "http://localhost:8080/api-keys/"+data_clean+"/activate"
     response = requests.post(url, headers=headers, timeout=5)
     response.raise_for_status()
 
 @app.route('/api-keys-deactivate', methods=['POST'])
 def deactivate_api_key():
-    url = f"http://localhost:8080/api-keys/{apiKeyDTO.get_api_key()}/deactivate"
+    data = json.dumps(request.get_json())
+    data_clean = data.replace('"',"")
+    url = "http://localhost:8080/api-keys/"+data_clean+"/deactivate"
     response = requests.post(url, headers=headers, timeout=5)
     response.raise_for_status()
 
