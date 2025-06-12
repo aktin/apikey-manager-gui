e1c327839b2d1a01ab563479624b6767f615f5f2
kombeizD <kombeiz.daniel@gmail.com>
2025-06-12 17:07:45 +0200

    added validation for apikey lenght

--- src/components/ClinicCredentialsInput.vue
+++ src/components/ClinicCredentialsInput.vue
@@ -9,35 +9,54 @@ import InputText from 'primevue/inputtext';
 import FloatLabel from "primevue/floatlabel";
 
 const blocked = ref(false);
-const value = ref(null);
+const apikeyinput = ref("");
+const commonName = ref(null);
+const organization = ref(null);
+const location = ref(null);
+const api_input_valid = ref(false)
+const is_api_valid = ref(false)
 
 const broker = new BrokerConnection();
+
+function makeInvalid() {
+  if(apikeyinput.value.length > 12) {
+    is_api_valid.value = false;
+  }
+  else
+  {is_api_valid.value = true;}
+
+}
+
 </script>
 
 <template>
-  <div  class="BG">
+  <div  class="bg-blue-200">
     <Button label="Block" @click="blocked = !blocked"></Button>
+    <div>
 
     <BlockUI :blocked="blocked">
-      <p>AAA</p>
-    </BlockUI>
-    <div>
-      <FloatLabel>
-        <InputText id="apikey_input" v-model="value" />
+
+      <FloatLabel class="FloatLabel">
+        <InputText id="apikey_input"  v-model="apikeyinput" :invalid = "is_api_valid" @update:modelValue="makeInvalid"/>
         <label for="apikey_input">ApiKey</label>
       </FloatLabel>
-      <FloatLabel>
-        <InputText id="commonName_input" v-model="value" />
+
+      <FloatLabel class="FloatLabel">
+        <InputText id="commonName_input" v-model="commonName" />
         <label for="commonName_input">Common Name</label>
       </FloatLabel>
-      <FloatLabel>
-        <InputText id="organization_input" v-model="value" />
+
+      <FloatLabel class="FloatLabel">
+        <InputText id="organization_input" v-model="organization" />
         <label for="organization_input">Organization</label>
       </FloatLabel>
-      <FloatLabel>
-        <InputText id="location_input" v-model="value" />
+
+      <FloatLabel class="FloatLabel mb-1 mb-5">
+        <InputText id="location_input" v-model="location" @update:modelValue="console.log(location)" />
         <label for="location_input">Location</label>
       </FloatLabel>
+    </BlockUI>
+
     </div>
   </div>
 </template>
@@ -46,5 +65,9 @@ const broker = new BrokerConnection();
 .BG
 {
   background-color: lightblue;
+}
+.FloatLabel
+{
+  margin-bottom: 30px;
 }
 </style>
