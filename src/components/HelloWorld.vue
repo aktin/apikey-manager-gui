b1b5cc9bcd2f60fdb0ea25899826d83d5530d603
kombeizD <kombeiz.daniel@gmail.com>
2025-06-12 15:22:02 +0200

    cleaned code up

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <script setup>
 import {computed, ref} from 'vue'
+import BrokerConnection from "./BrokerConnection.js";
 import Button from 'primevue/button';
 import Toast from 'primevue/toast';
 import {useToast} from "primevue/usetoast";
@@ -8,6 +9,8 @@ import {useToast} from "primevue/usetoast";
 defineProps({
   msg: String,
 })
+
+const broker = new BrokerConnection();
 
 const toast = useToast();
 const showToast = () => {
@@ -27,46 +30,6 @@ const clickBtnLabel = computed(() =>
     <h1>{{ msg }}</h1>
   </div>
 
-  <div class="flex flex-column ">
-    <div class="flex align-items-center justify-content-center">
-      <p>Demo PrimeVue Buttons</p>
-    </div>
-    <Button :label="clickBtnLabel" @click="count++" class="mb-2"/>
-    <Button label="I am a Toast" @click="showToast()"/>
-  </div>
-
-  <div class="flex flex-column">
-    <div class="flex align-items-center justify-content-center">
-      <p>Demo PrimeIcons</p>
-    </div>
-    <div class="flex align-items-center justify-content-center">
-      <i class="pi pi-spin pi-spinner" style="font-size: 2rem"></i>
-    </div>
-  </div>
-
-  <div class="flex flex-column">
-    <div class="flex align-items-center justify-content-center">
-      <p>Demo PrimeFlex Layout</p>
-    </div>
-    <div class="flex align-items-center justify-content-center">
-      <div class="grid nested-grid">
-        <div class="col-8">
-          <div class="grid">
-            <div class="col-6">
-              <div class="text-center p-3 border-round-sm bg-primary font-bold">6</div>
-            </div>
-            <div class="col-6">
-              <div class="text-center p-3 border-round-sm bg-primary font-bold">6</div>
-            </div>
-            <div class="col-12">
-              <div class="text-center p-3 border-round-sm bg-primary font-bold">12</div>
-            </div>
-          </div>
-        </div>
-        <div class="col-4">
-          <div class="text-center p-3 border-round-sm h-full bg-primary font-bold">4</div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <Button label="click here" @click="broker.create()"/>
+  <p id="bruh">test</p>
 </template>
