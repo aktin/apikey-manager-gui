2b1543486d6fe170ff408b27dfe9f1c9b8dc3fb4
akomii <alexkombie@gmail.com>
2025-06-12 10:51:00 +0200

    added prime stack to main.js, created little demo of prime stack

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,43 +1,62 @@
 <script setup>
-import { ref } from 'vue'
+import {computed, ref} from 'vue'
+import Button from 'primevue/button';
 
 defineProps({
   msg: String,
 })
 
 const count = ref(0)
+
+const clickBtnLabel = computed(() =>
+    count.value === 1 ? '1 Click' : `${count.value} Clicks`
+)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div class="flex align-items-center justify-content-center">
+    <h1>{{ msg }}</h1>
   </div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
-</template>
+  <div class="flex flex-column">
+    <div class="flex align-items-center justify-content-center">
+      <p>Demo PrimeVue Buttons</p>
+    </div>
+    <Button :label="clickBtnLabel" @click="count++"/>
+  </div>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
+  <div class="flex flex-column">
+    <div class="flex align-items-center justify-content-center">
+      <p>Demo PrimeIcons</p>
+    </div>
+    <div class="flex align-items-center justify-content-center">
+      <i class="pi pi-spin pi-spinner" style="font-size: 2rem"></i>
+    </div>
+  </div>
+
+  <div class="flex flex-column">
+    <div class="flex align-items-center justify-content-center">
+      <p>Demo PrimeFlex Layout</p>
+    </div>
+    <div class="flex align-items-center justify-content-center">
+      <div class="grid nested-grid">
+        <div class="col-8">
+          <div class="grid">
+            <div class="col-6">
+              <div class="text-center p-3 border-round-sm bg-primary font-bold">6</div>
+            </div>
+            <div class="col-6">
+              <div class="text-center p-3 border-round-sm bg-primary font-bold">6</div>
+            </div>
+            <div class="col-12">
+              <div class="text-center p-3 border-round-sm bg-primary font-bold">12</div>
+            </div>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="text-center p-3 border-round-sm h-full bg-primary font-bold">4</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// PrimeVue imports
+import PrimeVue from "primevue/config";
+
+// Styles imports
+import 'primevue/resources/themes/lara-light-green/theme.css'
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+
+const app = createApp(App)
+    .use(PrimeVue)
+    .mount("#app");
