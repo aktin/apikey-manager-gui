55312b06ed8ad9f9e88d3b72b86750f47a9b1caf
kombeizD <kombeiz.daniel@gmail.com>
2025-06-12 16:17:00 +0200

    created table for apikeys

---
+++ src/components/ClinicCredentialsTable.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import {computed, ref} from 'vue'
+import BrokerConnection from "./BrokerConnection.js";
+import Textarea from 'primevue/textarea';
+const value = ref("");
+</script>
+
+<template>
+  <Textarea v-model="value" rows="5" cols="30" />
+</template>
+
+<style scoped>
+
+</style>
