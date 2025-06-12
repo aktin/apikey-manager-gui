011f50aa41d20601fd33000187b2b5e39ce36c7e
kombeizD <kombeiz.daniel@gmail.com>
2025-06-12 15:37:54 +0200

    added all requests to backend

--- src/components/BrokerConnection.js
+++ src/components/BrokerConnection.js
@@ -5,17 +5,53 @@ class BrokerConnection {
     adminApiKey = "xxxAdmin1234";
 
     // TODO include vars instead of strings
-    async create() {
-        fetch("http://localhost:8080/api-keys", {
+    async getApiKeys() {
+        fetch(this.brokerUrl+"/api-keys", {
             method: "GET",
             headers: {
-                "Authorization": "Bearer xxxAdmin1234", "Content-Type": "application/json"
+                "Authorization": "Bearer "+this.adminApiKey, "Content-Type": "application/json"
             },
             mode: "cors"
         })
             .then(response => response.text())
             .then(data => console.log(data))
             .catch(error => console.error("Error:", error));
+    }
+
+    async addApiKeys(ClinicCredentials) {
+        fetch(this.brokerUrl+"/api-keys", {
+            method: "POST",
+            headers: {
+                "Authorization": "Bearer "+this.adminApiKey, "Content-Type": "application/xml"
+            },
+            body: JSON.stringify({ClinicCredentials})
+        })
+            .then(response => response.text())
+            .then(data => console.log(data))
+            .catch(error => console.error("Error:", error))
+    }
+
+    async activateApiKey(apiKey) {
+        fetch(this.brokerUrl+"/api-keys/"+apiKey+"/activate", {
+            method: "POST",
+            headers: {
+                "Authorization": "Bearer "+this.adminApiKey
+            },
+        })
+            .then(response => response.text())
+            .then(data => console.log(data))
+            .catch(error => console.error("Error:", error))
+    }
+    async deactivateApiKey(apiKey) {
+        fetch(this.brokerUrl+"/api-keys/"+apiKey+"/deactivate", {
+            method: "POST",
+            headers: {
+                "Authorization": "Bearer "+this.adminApiKey
+            },
+        })
+            .then(response => response.text())
+            .then(data => console.log(data))
+            .catch(error => console.error("Error:", error))
     }
 }
 
