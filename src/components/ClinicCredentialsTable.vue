2901bf17f8f316b29fb391bd2a97e08617547536
kombeizD <kombeiz.daniel@gmail.com>
2025-06-13 15:45:28 +0200

    added working table

--- src/components/ClinicCredentialsTable.vue
+++ src/components/ClinicCredentialsTable.vue
@@ -1,12 +1,41 @@
-<script setup lang="ts">
-import {computed, ref} from 'vue'
+<script setup>
+import {ref} from 'vue'
 import BrokerConnection from "./BrokerConnection.js";
-import Textarea from 'primevue/textarea';
-const value = ref("");
+import Button from 'primevue/button';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+
+const broker = new BrokerConnection();
+const bruh = ref("");
+
+async function showlist()
+{
+  bruh.value = await broker.getApiKeys()
+}
+
+
+const products = ref([
+  { ApiKey: 'xxxAPIKEY111', CommonName: 'Clinic A', Organization: 'ORG B', Location: "place D" },
+  { ApiKey: 'xxxAPIKEY222', CommonName: 'Clinic B', Organization: 'ORG C', Location: "place A" },
+  { ApiKey: 'xxxAPIKEY333', CommonName: 'Clinic C', Organization: 'ORG D', Location: "place B" },
+  { ApiKey: 'xxxAPIKEY444', CommonName: 'Clinic D', Organization: 'ORG A', Location: "place C" },
+]);
+
 </script>
 
 <template>
-  <Textarea v-model="value" rows="5" cols="30" />
+
+  <Button label="get api list" @click="showlist()"/>
+
+  <div class="card border-1">
+    <DataTable :value="products" tableStyle="width: 50%">
+      <Column field="ApiKey" header="ApiKey" style="width: 25px"></Column>
+      <Column field="CommonName" header="Common name" sortable style="width: 25px"></Column>
+      <Column field="Organization" header="Organization" sortable style="width: 25px"></Column>
+      <Column field="Location" header="Location" sortable style="width: 25px"></Column>
+    </DataTable>
+  </div>
+
 </template>
 
 <style scoped>
