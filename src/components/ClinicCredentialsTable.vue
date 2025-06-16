f80162335107048dbcb62280ce46b3b247f28281
kombeizD <kombeiz.daniel@gmail.com>
2025-06-16 17:06:49 +0200

    apikeys in list can now be selected, saves selected apikey

--- src/components/ClinicCredentialsTable.vue
+++ src/components/ClinicCredentialsTable.vue
@@ -1,31 +1,35 @@
 <script setup>
-import {ref} from 'vue'
+import {ref, toRaw, watch} from 'vue'
 import BrokerConnection from "./BrokerConnection.js";
-import Button from 'primevue/button';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
 
 const broker = new BrokerConnection();
 const selectedApiKey = ref("");
+const apiKeyList = ref();
+
+const selectedRow = ref(null);
 
 const init = () => { showlist()}
 
 async function showlist()
 {
-  products.value = await broker.getApiKeys()
+  apiKeyList.value = await broker.getApiKeys()
 }
 
+watch(selectedApiKey, (newVal) => {
+  if(newVal) {
+    console.log(toRaw(newVal).ApiKey);
+  }
+});
 
-const products = ref([]);
 init();
 </script>
 
 <template>
 
-  <Button label="get api list" @click="showlist()"/>
-
   <div class="card border-1">
-    <DataTable v-model:selection="selectedApiKey" :value="products" selectionMode="single" :meta-key-selection="false" tableStyle="width: 100%" scrollable scrollHeight="450px">
+    <DataTable v-model:selection="selectedApiKey" :value="apiKeyList" selectionMode="single" :meta-key-selection="false" tableStyle="width: 100%" scrollable scrollHeight="450px">
       <Column field="ApiKey" header="ApiKey" style="width: 20%"></Column>
       <Column field="CommonName" header="Common name" sortable style="width: 20%"></Column>
       <Column field="Organization" header="Organization" sortable style="width: 20%"></Column>
