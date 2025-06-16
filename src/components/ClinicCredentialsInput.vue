fd44cd1008555b2d9b71cf2092ac9af332e45063
kombeizD <kombeiz.daniel@gmail.com>
2025-06-16 17:07:13 +0200

    changed formating

--- src/components/ClinicCredentialsInput.vue
+++ src/components/ClinicCredentialsInput.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import {ref} from "vue";
 import Button from "primevue/button";
 import BlockUI from "primevue/blockui";
 import InputText from "primevue/inputtext";
 import FloatLabel from "primevue/floatlabel";
+import BrokerConnection from "./BrokerConnection.js";
+
+const broker = new BrokerConnection();
 
 // Reactive variables
 const commonNameInput = ref("");
@@ -11,7 +14,6 @@ const blocked = ref(false);
 const apiKeyInput = ref("");
 const organizationInput = ref("");
 const locationInput = ref("");
-
 
 
 let apiKeyErrorText = ref("");
@@ -23,7 +25,6 @@ const isApiKeyInvalid = ref(false);
 const isCommonNameInvalid = ref(false);
 const isOrganizationInvalid = ref(false);
 const isLocationInvalid = ref(false);
-
 
 
 function validate() {
@@ -44,63 +45,73 @@ function validate() {
       errorText.value = "";
     }
   }
+
   checkInput(apiKeyInput, isApiKeyInvalid, apiKeyErrorText, true);
   checkInput(commonNameInput, isCommonNameInvalid, commonNameErrorText);
   checkInput(organizationInput, isOrganizationInvalid, organizationErrorText);
   checkInput(locationInput, isLocationInvalid, locationErrorText);
 }
+
+function changeState() {
+
+}
+
 </script>
 
 <template>
-  <div class="bg-gray-200">
-    <Button label="Block" @click="blocked = !blocked"></Button>
+  <div class="bg-gray-300">
 
     <BlockUI :blocked="blocked">
 
-      <div class="field grid mt-5">
-        <div class="col">
+      <div class="field grid">
+        <div class="input_Div mt-3">
           <FloatLabel>
-            <InputText id="apiInput" type="text" class="text-base text-color surface-overlay p-2 " v-model="apiKeyInput"  :invalid="isApiKeyInvalid"/>
+            <InputText id="apiInput" type="text" class="text-base text-color surface-overlay p-2 input_Field" v-model="apiKeyInput"
+                       :invalid="isApiKeyInvalid"/>
             <label for="apiInput" class="col-fixed">api key</label>
           </FloatLabel>
         </div>
-        <label id="apiKeyErrorText" for="apiInput" class="col-fixed" style="width:300px">{{apiKeyErrorText}}</label>
+        <label id="apiKeyErrorText" for="apiInput" class="errorLabel" >{{ apiKeyErrorText }}</label>
       </div>
-
-      <div class="field grid mt-5 ">
-        <div class="col ">
+      <div class="input_Div">
+        <Button>generate ApiKey</Button>
+      </div>
+      <div class="field grid mt-4">
+        <div class="input_Div">
           <FloatLabel>
-            <InputText id="nameInput" type="text" class="text-base text-color surface-overlay p-2 " v-model="commonNameInput" :invalid="isCommonNameInvalid"/>
+            <InputText id="nameInput" type="text" class="text-base text-color surface-overlay p-2 input_Field"
+                       v-model="commonNameInput" :invalid="isCommonNameInvalid"/>
             <label for="nameInput" class="col-fixed">common name</label>
           </FloatLabel>
         </div>
-        <label id="commonNameErrorText" for="nameInput" class="col-fixed" style="width:300px">{{ commonNameErrorText }}</label>
+        <label id="commonNameErrorText" for="nameInput" class="errorLabel" >{{commonNameErrorText }}</label>
       </div>
 
-      <div class="field grid mt-5">
-        <div class="col">
+      <div class="field grid ">
+        <div class="input_Div">
           <FloatLabel>
-            <InputText id="orgInput" type="text" class="text-base text-color surface-overlay p-2 " v-model="organizationInput" :invalid="isOrganizationInvalid"/>
+            <InputText id="orgInput" type="text" class="text-base text-color surface-overlay p-2 input_Field"
+                       v-model="organizationInput" :invalid="isOrganizationInvalid"/>
             <label for="orgInput" class="col-fixed">organization</label>
           </FloatLabel>
         </div>
-        <label id="organizationErrorText" for="orgInput" class="col-fixed" style="width:300px">{{ organizationErrorText }}</label>
+        <label id="organizationErrorText" for="orgInput" class="errorLabel" >{{organizationErrorText }}</label>
       </div>
 
-      <div class="field grid mt-5">
-        <div class="col">
+      <div class="field grid ">
+        <div class="input_Div">
           <FloatLabel>
-            <InputText id="locInput" type="text" class="text-base text-color surface-overlay p-2 " v-model="locationInput" :invalid="isLocationInvalid"/>
-            <label for="locInput" class="col-fixed">location</label>
+            <InputText id="locInput" type="text" class="text-base text-color surface-overlay p-2 input_Field"
+                       v-model="locationInput" :invalid="isLocationInvalid"/>
+            <label for="locInput">location</label>
           </FloatLabel>
         </div>
-        <label id="locationErrorText" for="locInput" class="col-fixed" style="width:300px">{{ locationErrorText }}</label>
+        <label id="locationErrorText" for="locInput" class="errorLabel" >{{locationErrorText }}</label>
       </div>
       <Button label="add" @click="validate()"></Button>
+      <Button label="deactivate" @click="changeState()"></Button>
     </BlockUI>
   </div>
-
-
 </template>
 
 <style scoped>
@@ -113,8 +124,21 @@ function validate() {
 .p-inputtext:hover {
   border-color: initial !important;
 }
+
 button:focus {
   box-shadow: none !important;
 }
 
+.errorLabel {
+  width:50%;
+  margin-left:auto;
+}
+.input_Div{
+  width:50%;
+  flex-grow:1;
+  padding: 15px
+}
+.input_Field{
+  width:100%;
+}
 </style>
