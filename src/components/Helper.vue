099805109b246399b0e455a8d09a5c536133ba8c
kombeizD <kombeiz.daniel@gmail.com>
2025-06-17 10:53:39 +0200

    created helper file

---
+++ src/components/Helper.vue
@@ -0,0 +1,18 @@
+<script setup>
+import BrokerConnection from "./BrokerConnection.js";
+const broker = new BrokerConnection();
+
+async function getlist()
+{
+  apiKeyList.value = await broker.getApiKeys()
+}
+
+</script>
+
+<template>
+  
+</template>
+
+<style scoped>
+
+</style>
