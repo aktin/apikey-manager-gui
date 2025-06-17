bf27e8c7a861d943dac7f9a61189c4fb43989789
kombeizD <kombeiz.daniel@gmail.com>
2025-06-17 11:49:53 +0200

    list of apikeys now over helper

--- src/components/Helper.js
+++ src/components/Helper.js
@@ -1,6 +1,33 @@
 import BrokerConnection from "./BrokerConnection.js";
 const broker = new BrokerConnection();
 
+class Helper{
 
+    apiKeyList;
+
+    async fetchlist()
+    {
+        this.apiKeyList = await broker.getApiKeys()
+        console.log(this.apiKeyList)
+        return this.formatIntoList(this.apiKeyList);
+    }
+
+    formatIntoList(textBlock)
+    {
+        return textBlock
+            .trim()
+            .split("\n")
+            .map(element => {
+                const [apiKey, , commonName, , organization, , location, status = "ACTIVE"] = element.split(/[=,]/);
+                return {
+                    ApiKey: apiKey,
+                    CommonName: commonName,
+                    Organization: organization,
+                    Location: location,
+                    Status: status
+                };
+            });
+    }
+}
 
 export default Helper;
