b74a1b8a99f27a5a44c2579cf6af89e3a7dc872f
kombeizD <kombeiz.daniel@gmail.com>
2025-06-17 15:47:09 +0200

    selected ApiKey will be sent to Inputs

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <script setup>
+import { ref } from 'vue';
 import BrokerConnectionChecker from './components/BrokerConnectionChecker.vue'
 import ClinicCredentialsInput from "./components/ClinicCredentialsInput.vue";
 import ClinicCredentialsTable from "./components/ClinicCredentialsTable.vue";
+
+const selectedApiKey = ref("");
+
+const handleApiKeyUpdate = (newApiKey) => {
+  selectedApiKey.value = newApiKey;
+};
 </script>
 
 <template>
   <div class="m-3">
     <div class="grid">
-     <ClinicCredentialsTable @update:selectedApiKey="console.log($event)" class="col-9 border-1"/>
+     <ClinicCredentialsTable @update:selectedApiKey="handleApiKeyUpdate" class="col-9 border-1"/>
       <div class="col-3">
         <BrokerConnectionChecker class="border-1"/>
-        <!-- <ClinicCredentialsInput/> -->
+        <ClinicCredentialsInput :selectedKey="selectedApiKey"/>
       </div>
     </div>
   </div>
