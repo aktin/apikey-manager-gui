af0d3182628dcccb9b2ff0671f1c5a2774620469
kombeizD <kombeiz.daniel@gmail.com>
2025-06-18 15:28:50 +0200

    sends connection status to form

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,32 @@
 <script setup>
 import { ref } from 'vue';
 import BrokerConnectionChecker from './components/BrokerConnectionChecker.vue'
-import ClinicCredentialsInput from "./components/ClinicCredentialsInput.vue";
+import ClinicCredentialsForm from "./components/ClinicCredentialsForm.vue";
 import ClinicCredentialsTable from "./components/ClinicCredentialsTable.vue";
 import Toast from "primevue/toast";
 
-const selectedApiKey = ref("");
+const table = ref(null)
 
+const selectedApiKey = ref("");
 const handleApiKeyUpdate = (newApiKey) => {
   selectedApiKey.value = newApiKey;
 };
+
+const isConnected = ref(true);
+const handleConnectionUpdate = (newConnectionStatus) => {
+  isConnected.value = newConnectionStatus;
+};
+
 </script>
 
 <template>
   <Toast/>
   <div class="m-3">
     <div class="grid">
-     <ClinicCredentialsTable @update:selectedApiKey="handleApiKeyUpdate" class="col-9 border-1"/>
+     <ClinicCredentialsTable ref="table" @update:selectedApiKey="handleApiKeyUpdate" class="col-9 border-1"/>
       <div class="col-3">
-        <BrokerConnectionChecker class="border-1"/>
-        <ClinicCredentialsInput :selectedKey="selectedApiKey"/>
+        <BrokerConnectionChecker @update:isConnected="handleConnectionUpdate" class="border-1"/>
+        <ClinicCredentialsForm :selectedKey="selectedApiKey" :connectionStatus="isConnected" />
       </div>
     </div>
   </div>
