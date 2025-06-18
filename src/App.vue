2649675f4954fade1c492c5d7c5d1e119a4a3eb7
kombeizD <kombeiz.daniel@gmail.com>
2025-06-18 15:29:16 +0200

    removed redundancies

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,6 @@ import ClinicCredentialsForm from "./components/ClinicCredentialsForm.vue";
 import ClinicCredentialsTable from "./components/ClinicCredentialsTable.vue";
 import Toast from "primevue/toast";
 
-const table = ref(null)
-
 const selectedApiKey = ref("");
 const handleApiKeyUpdate = (newApiKey) => {
   selectedApiKey.value = newApiKey;
@@ -23,7 +21,7 @@ const handleConnectionUpdate = (newConnectionStatus) => {
   <Toast/>
   <div class="m-3">
     <div class="grid">
-     <ClinicCredentialsTable ref="table" @update:selectedApiKey="handleApiKeyUpdate" class="col-9 border-1"/>
+     <ClinicCredentialsTable @update:selectedApiKey="handleApiKeyUpdate" class="col-9 border-1"/>
       <div class="col-3">
         <BrokerConnectionChecker @update:isConnected="handleConnectionUpdate" class="border-1"/>
         <ClinicCredentialsForm :selectedKey="selectedApiKey" :connectionStatus="isConnected" />
