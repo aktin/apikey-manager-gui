6cd6012685ff87822b849e21e5fb2b603d697b23
akomii <alexkombie@gmail.com>
2025-06-20 12:33:23 +0200

    added ToDo, formatting

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <script setup>
-import { ref } from 'vue';
+import {ref} from 'vue';
 import BrokerConnectionChecker from './components/BrokerConnectionChecker.vue'
 import ClinicCredentialsForm from "./components/ClinicCredentialsForm.vue";
 import ClinicCredentialsTable from "./components/ClinicCredentialsTable.vue";
 import Toast from "primevue/toast";
 
 const selectedApiKey = ref("");
+const isConnected = ref(true);
+
 const handleApiKeyUpdate = (newApiKey) => {
   selectedApiKey.value = newApiKey;
 };
 
-const isConnected = ref(true);
 const handleConnectionUpdate = (newConnectionStatus) => {
   isConnected.value = newConnectionStatus;
 };
-
 </script>
 
 <template>
   <Toast/>
   <div class="m-3">
     <div class="grid">
-     <ClinicCredentialsTable @update:selectedApiKey="handleApiKeyUpdate" class="col-9 border-1"/>
+      <ClinicCredentialsTable @update:selectedApiKey="handleApiKeyUpdate" class="col-9 border-1"/>
       <div class="col-3">
         <BrokerConnectionChecker @update:isConnected="handleConnectionUpdate" class="border-1"/>
-        <ClinicCredentialsForm :selectedKey="selectedApiKey" :connectionStatus="isConnected" />
+        <ClinicCredentialsForm :selectedKey="selectedApiKey" :connectionStatus="isConnected"/>
       </div>
     </div>
   </div>
--- src/components/ClinicCredentialsForm.vue
+++ src/components/ClinicCredentialsForm.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-import {ref} from "vue";
+import {defineProps, ref} from "vue";
 import Button from "primevue/button";
 import BlockUI from "primevue/blockui";
 import InputText from "primevue/inputtext";
 import FloatLabel from "primevue/floatlabel";
 import BrokerConnection from "./BrokerConnection.js";
-import { defineProps } from 'vue';
 
-import { useToast } from "primevue/usetoast";
+import {useToast} from "primevue/usetoast";
+
 const toast = useToast();
 const toastLife = 1000 * 5;
 
@@ -18,7 +18,6 @@ const apiKeyInput = ref("");
 const organizationInput = ref("");
 const locationInput = ref("");
 
-
 const isApiKeyInvalid = ref(false);
 const isCommonNameInvalid = ref(false);
 const isOrganizationInvalid = ref(false);
@@ -28,45 +27,46 @@ const props = defineProps({
   selectedKey: String, connectionStatus: Boolean
 });
 
-async function addApikey(){
+async function addApikey() {
   validate();
-  if(!isApiKeyInvalid.value && !isCommonNameInvalid.value && !isOrganizationInvalid.value && !isLocationInvalid.value)
-  {
+  if (!isApiKeyInvalid.value && !isCommonNameInvalid.value && !isOrganizationInvalid.value && !isLocationInvalid.value) {
     const payload = "CN=" + commonNameInput.value + ",O=" + organizationInput.value + ",L=" + locationInput.value;
     const xml_data = "<ApiKeyCred><apiKey>" + apiKeyInput.value + "</apiKey><clientDn>" + payload + "</clientDn></ApiKeyCred>";
     const statusCode = await broker.addApiKeys(xml_data)
-    if(statusCode === 409) {
-      createErrorToast("Conflict","Api Key already exists");
-    }else if(statusCode === 500){
-      createErrorToast("Connection Error","Could not send Api Key. Code:500");
-    }else if(statusCode === 201){
+    if (statusCode === 409) {
+      createErrorToast("Conflict", "Api Key already exists");
+    } else if (statusCode === 500) {
+      createErrorToast("Connection Error", "Could not send Api Key. Code:500");
+    } else if (statusCode === 201) {
       createSuccessToast("Api Key has been added")
-    }else{
-      createErrorToast("Unknown Error","An unknown error occurred. Code:"+statusCode);
+    } else {
+      createErrorToast("Unknown Error", "An unknown error occurred. Code:" + statusCode);
     }
   }
 }
 
+
+//TODO DN should be able to include , ; . : - _
 function validateField(value, inputField) {
   const pattern = /[!@#$%^&*(),.?":{}|<>_-]/;
   if (value === '') {
-    createErrorToast("Input Error",inputField+" cannot be empty");
+    createErrorToast("Input Error", inputField + " cannot be empty");
     return true;
   } else if (pattern.test(value)) {
-    createErrorToast("Input Error",inputField+" cannot contain special symbols");
+    createErrorToast("Input Error", inputField + " cannot contain special symbols");
     return true;
   }
   return false;
 }
 
-function validate(){
+function validate() {
 
   if (apiKeyInput.value.length !== 12) {
-    createErrorToast("Input Error","Api Key must be 12 characters");
+    createErrorToast("Input Error", "Api Key must be 12 characters");
     isApiKeyInvalid.value = true;
 
   } else if (/[!@#$%^&*(),.?":{}|<>_-]/.test(apiKeyInput.value)) {
-    createErrorToast("Input Error","Api Key cannot contain special symbols");
+    createErrorToast("Input Error", "Api Key cannot contain special symbols");
     isApiKeyInvalid.value = true;
 
   } else {
@@ -89,34 +89,30 @@ function validate(){
   );
 }
 
-function createErrorToast(title,detail){
-  toast.add({severity:"error",summary:title, detail , life:toastLife})
+function createErrorToast(title, detail) {
+  toast.add({severity: "error", summary: title, detail, life: toastLife})
 }
 
-function createSuccessToast(detail){
-  toast.add({severity:"success",summary:"Success", detail , life:toastLife})
+function createSuccessToast(detail) {
+  toast.add({severity: "success", summary: "Success", detail, life: toastLife})
 }
 
-async function changeState()
-{
+async function changeState() {
   const statusOfSelectedApiKey = props.selectedKey.split(";")[1]
 
   const selectedApiKey = props.selectedKey.split(";")[0]
 
-  if(statusOfSelectedApiKey === "INACTIVE"){
+  if (statusOfSelectedApiKey === "INACTIVE") {
     await broker.activateApiKey(selectedApiKey)
-  }
-  else if(statusOfSelectedApiKey === "ACTIVE"){
+  } else if (statusOfSelectedApiKey === "ACTIVE") {
     await broker.deactivateApiKey(selectedApiKey)
   }
 }
 
-function generateApiKey()
-{
+function generateApiKey() {
   const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let key = '';
-  for (let i = 0; i < 12; i++)
-  {
+  for (let i = 0; i < 12; i++) {
     key += chars.charAt(Math.floor(Math.random() * chars.length));
   }
   apiKeyInput.value = key;
@@ -141,28 +137,31 @@ function generateApiKey()
         <Button @click="generateApiKey()">generate ApiKey</Button>
       </div>
       <div class="field grid mt-4 p-3">
-          <FloatLabel>
-            <InputText id="nameInput" type="text" class="text-base text-color surface-overlay p-2 input_Field" v-model="commonNameInput" :invalid="isCommonNameInvalid"/>
-            <label for="nameInput" class="col-fixed">common name</label>
-          </FloatLabel>
+        <FloatLabel>
+          <InputText id="nameInput" type="text" class="text-base text-color surface-overlay p-2 input_Field" v-model="commonNameInput"
+                     :invalid="isCommonNameInvalid"/>
+          <label for="nameInput" class="col-fixed">common name</label>
+        </FloatLabel>
       </div>
 
       <div class="field grid p-3">
-          <FloatLabel>
-            <InputText id="orgInput" type="text" class="text-base text-color surface-overlay p-2 input_Field" v-model="organizationInput" :invalid="isOrganizationInvalid"/>
-            <label for="orgInput" class="col-fixed">organization</label>
-          </FloatLabel>
+        <FloatLabel>
+          <InputText id="orgInput" type="text" class="text-base text-color surface-overlay p-2 input_Field" v-model="organizationInput"
+                     :invalid="isOrganizationInvalid"/>
+          <label for="orgInput" class="col-fixed">organization</label>
+        </FloatLabel>
       </div>
 
       <div class="field grid p-3">
-          <FloatLabel>
-            <InputText id="locInput" type="text" class="text-base text-color surface-overlay p-2 input_Field" v-model="locationInput" :invalid="isLocationInvalid"/>
-            <label for="locInput">location</label>
-          </FloatLabel>
+        <FloatLabel>
+          <InputText id="locInput" type="text" class="text-base text-color surface-overlay p-2 input_Field" v-model="locationInput"
+                     :invalid="isLocationInvalid"/>
+          <label for="locInput">location</label>
+        </FloatLabel>
       </div>
 
       <div class="flex gap-3 p-3">
-         <Button label="add" @click="addApikey()"></Button>
+        <Button label="add" @click="addApikey()"></Button>
         <div v-if=" props.selectedKey.split(';')[1]  ==='ACTIVE' " class=" flex align-items-center text-green-600 text-xl">
           <Button label="deactivate" @click="changeState()"></Button>
         </div>
@@ -189,7 +188,7 @@ button:focus {
   box-shadow: none !important;
 }
 
-.input_Field{
-  width:100%;
+.input_Field {
+  width: 100%;
 }
 </style>
