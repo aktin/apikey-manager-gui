b98b89665c2f9bbe36dd6b110f16ac730c3d882d
kombeizD <kombeiz.daniel@gmail.com>
2025-06-24 14:42:37 +0200

    added login

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,13 +1,30 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
+import InputText from "primevue/inputtext";
+import FloatLabel from "primevue/floatlabel";
+import {ref} from "vue";
+import { updateValue } from '../router.js';
+
 const router = useRouter();
 
+const password = ref("");
+
 const goToPage = () => {
-  router.push('/')
+  router.push('/app')
+}
+
+function validateInput(){
+  if (password.value === "xxxAdmin1234") {
+    updateValue(password.value)
+    goToPage();
+  }
 }
 </script>
 
 <template>
-  <p style="color:red ; font-size:30px ; outline: #FF0000 solid 10px;">login screen</p>
-  <Button @click="goToPage">go to home</Button>
+  <FloatLabel>
+    <InputText id="passwordInput" type="text" class="text-base text-color surface-overlay p-2 input_Field" v-model="password"/>
+    <label for="passwordInput" class="col-fixed">Password</label>
+  </FloatLabel>
+  <Button @click="validateInput">log in</Button>
 </template>
