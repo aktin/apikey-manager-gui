18081441b75f50a73a53e7770d8fd131ceb10b8d
kombeizD <kombeiz.daniel@gmail.com>
2025-06-24 14:47:37 +0200

    sends password for redirecting

--- src/router.js
+++ src/router.js
@@ -1,15 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import home from './components/home.vue'
 import login from './components/login.vue'
+import {sharedPassword} from './components/passwordChanger.js'
 
 const routes = [
-    { path: '/', component: home },
-    { path: '/login', component: login }
+    { path: '/app', component: home },
+    { path: '/', component: login }
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes
 })
+
+export let password = "";
+export function updateValue(newPassword) {
+    password = newPassword;
+    console.log("new Password : ",password);
+    sharedPassword.value = password;
+}
 
 export default router
