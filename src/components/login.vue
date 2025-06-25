3c6573cb9f037e26211d50e6b060eb6bbe593f95
kombeizD <kombeiz.daniel@gmail.com>
2025-06-25 12:59:17 +0200

    refactored to use credentials instead of cookies

--- src/components/login.vue
+++ src/components/login.vue
@@ -4,7 +4,8 @@ import InputText from "primevue/inputtext";
 import FloatLabel from "primevue/floatlabel";
 import {onMounted, ref} from "vue";
 import { updateLoginValues } from '../router.js';
-
+import Password from 'primevue/password';
+import BrokerConnection from "./BrokerConnection.js";
 import Button from "primevue/button";
 
 const router = useRouter();
@@ -17,54 +18,23 @@ const goToPage = () => {
 }
 
 function logIn(){
-  setCookie("password",password.value,5)
-  setCookie("url",url.value,5)
+  BrokerConnection.setCredentials(url.value, password.value)
   validateInput()
 }
 
 function validateInput(){
-  if (password.value === "xxxAdmin1234" && url.value!="") {
-    updateLoginValues(password.value , url.value)
+
+  console.log("URL :",BrokerConnection.getCredentials().url)
+  console.log("KEY :",BrokerConnection.getCredentials().key)
+
+  if (BrokerConnection.getCredentials().key ==="xxxAdmin1234") {
+    updateLoginValues(BrokerConnection.getCredentials().key)
     goToPage();
   }
 }
 
-function setCookie(key: string, value: string,exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + (exdays*60*1000));
-  let expires = "expires="+ d.toUTCString();
-  document.cookie = key + "=" + value + ";" + expires + ";secure"
-}
-
-function getCookie(key) {
-  let name = key + "=";
-  let cookieList = document.cookie.split(';');
-  for(let i = 0; i < cookieList.length; i++) {
-    let eachCookie = cookieList[i];
-    while (eachCookie.charAt(0) == ' ') {
-      eachCookie = eachCookie.substring(1);
-    }
-    if (eachCookie.indexOf(name) == 0) {
-      return eachCookie.substring(name.length, eachCookie.length);
-    }
-  }
-  return "";
-}
-
-function checkCookie() {
-  let enteredPassword =  getCookie("password");
-  let enteredUrl =  getCookie("url");
-  if (enteredUrl!="" && enteredPassword!="") {
-    password.value = enteredPassword;
-    url.value = enteredUrl;
-  }
-}
-
 onMounted(() => {
-  checkCookie();
-  if(password.value && url.value) {
     validateInput()
-  }
 });
 </script>
 
@@ -73,7 +43,7 @@ onMounted(() => {
     <div class="field grid flex justify-content-center flex-wrap">
       <div class="p-3 mt-3">
         <FloatLabel>
-          <InputText id="passwordInput" type="text" class="text-base text-color surface-overlay p-2 input_Field" v-model="password"/>
+          <Password id="passwordInput" v-model="password" size="small" toggleMask :feedback="false"/>
           <label for="passwordInput" class="col-fixed">Password</label>
         </FloatLabel>
       </div>
