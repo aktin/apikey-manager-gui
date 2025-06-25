6612d3b1cde92ea39ca2b9eeffef12b475aacadc
kombeizD <kombeiz.daniel@gmail.com>
2025-06-25 10:33:50 +0200

    added cookies to check if already logged in

--- src/components/login.vue
+++ src/components/login.vue
@@ -2,7 +2,7 @@
 import { useRouter } from 'vue-router'
 import InputText from "primevue/inputtext";
 import FloatLabel from "primevue/floatlabel";
-import {ref} from "vue";
+import {onMounted, ref} from "vue";
 import { updateLoginValues } from '../router.js';
 
 import Button from "primevue/button";
@@ -19,9 +19,46 @@ const goToPage = () => {
 function validateInput(){
   if (password.value === "xxxAdmin1234" && url.value!="") {
     updateLoginValues(password.value , url.value)
+    setCookie("password",password.value)
+    setCookie("url",url.value)
     goToPage();
   }
 }
+
+function setCookie(key: string, value: string) {
+  document.cookie = key + "=" + value
+}
+
+function getCookie(key) {
+  let name = key + "=";
+  let cookieList = document.cookie.split(';');
+  for(let i = 0; i < cookieList.length; i++) {
+    let eachCookie = cookieList[i];
+    while (eachCookie.charAt(0) == ' ') {
+      eachCookie = eachCookie.substring(1);
+    }
+    if (eachCookie.indexOf(name) == 0) {
+      return eachCookie.substring(name.length, eachCookie.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie() {
+  let enteredPassword =  getCookie("password");
+  let enteredUrl =  getCookie("url");
+  if (enteredUrl!="" && enteredPassword!="") {
+    password.value = enteredPassword;
+    url.value = enteredUrl;
+  }
+}
+
+onMounted(() => {
+  checkCookie();
+  if(password.value && url.value) {
+    validateInput()
+  }
+});
 </script>
 
 <template>
