4383544ea796f35ee262cb11dbf42acf920beab5
kombeizD <kombeiz.daniel@gmail.com>
2025-06-26 15:15:30 +0200

    reconfigured electrons

--- my-vue-app/src/main.js
+++ my-vue-app/src/main.js
@@ -17,11 +17,10 @@ const createWindow = () => {
         },
     });
 
-    // and load the index.html of the app.
     if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
         mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
     } else {
-        mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/App.vue`));
+        mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
     }
 };
 
--- my-vue-app/src/renderer.js
+++ my-vue-app/src/renderer.js
@@ -1,4 +1,22 @@
-import { createApp } from 'vue';
-import App from './App.vue';
+import {createApp} from "vue"
+import App from "./App.vue"
+import router from "./router"
 
-createApp(App).mount('#app');
+// PrimeVue imports
+import PrimeVue from "primevue/config";
+import ToastService from "primevue/toastservice";
+import Tooltip from "primevue/tooltip";
+
+// Styles imports
+import "primevue/resources/themes/lara-light-green/theme.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+
+const app = createApp(App)
+    .use(PrimeVue)
+    .use(ToastService)
+    .use(router)
+    .directive("tooltip", Tooltip)
+    .mount("#app");
+
+//TODO (Optional) Add locale for 'de' and 'en'
