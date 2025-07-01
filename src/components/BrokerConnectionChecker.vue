702db07298e4198f6f3270faac50a206d920202e
kombeizD <kombeiz.daniel@gmail.com>
2025-07-01 10:50:51 +0200

    save Button disables if no changes

--- src/components/BrokerConnectionChecker.vue
+++ src/components/BrokerConnectionChecker.vue
@@ -13,7 +13,8 @@ import ConfirmPopup from 'primevue/confirmpopup';
 import {useConfirm} from "primevue/useconfirm";
 
 const visible = ref(false);
-const disabled = ref(false);
+const deleteDisabled = ref(true);
+const saveDisabled = ref(true);
 
 const selectedCredentials = ref();
 const savedCredentials = ref([]);
@@ -25,6 +26,14 @@ const emit = defineEmits(["update:isConnected"]);
 const userName = ref("");
 const password = ref("");
 const url = ref("");
+
+const savedName = ref("");
+const savedPassword = ref("");
+const savedUrl = ref("");
+
+const nameNotChanged = ref(true);
+const passwordNotChanged = ref(true);
+const urlNotChanged = ref(true);
 
 const connected = ref(false);
 
@@ -46,8 +55,13 @@ async function checkConnection() {
   sendConnectionStatus(status.value);
 }
 
-async function insertCredentials(nameValue) {
+function changeSavedCreds() {
+  savedName.value = userName.value;
+  savedPassword.value = password.value;
+  savedUrl.value = url.value;
+}
 
+async function insertCredentials(nameValue) {
   const credentialsRaw = await window.storeAPI.get(nameValue)
 
   if (credentialsRaw) {
@@ -55,14 +69,16 @@ async function insertCredentials(nameValue) {
     userName.value = credentialsList[0];
     password.value = credentialsList[1];
     url.value = credentialsList[2];
-    disabled.value = false
+    deleteDisabled.value = false
   } else {
     selectedCredentials.value = "";
     userName.value = "";
     password.value = "";
     url.value = "";
-    disabled.value = true
+    deleteDisabled.value = true
   }
+  changeSavedCreds()
+  allInputChanged()
   BrokerConnection.setCredentials(url.value, password.value)
   window.storeAPI.set("LastSelected", nameValue);
   await window.callVueFunction();
@@ -95,7 +111,7 @@ async function loadCredentialList() {
   })
 }
 
-function saveCredentials() {
+async function saveCredentials() {
   if (!url.value.startsWith('http://')) {
     url.value = "http://" + url.value
   }
@@ -106,21 +122,29 @@ function saveCredentials() {
     window.storeAPI.set(userName.value, combined);
     loadCredentialList()
     selectedCredentials.value = userName.value;
+    changeSavedCreds();
+    allInputChanged()
+    await insertCredentials(userName.value);
   }
 }
 
 async function deleteCredentials() {
+
+  saveDisabled.value = true;
+
   const toDelete = selectedCredentials.value.name
   await window.storeAPI.delete(toDelete);
   toast.add({severity: 'success', summary: 'Confirmed', detail: toDelete + ' deleted', life: toastLife});
   await loadCredentialList()
 
-  if(savedCredentials.value[0]) {
+  if (savedCredentials.value[0]) {
     selectedCredentials.value.name = savedCredentials.value[0].name
     await insertCredentials(selectedCredentials.value.name)
-  }else{
+  } else {
     await insertCredentials("")
   }
+  changeSaveButton();
+  allInputChanged()
 }
 
 const confirmDelete = (event) => {
@@ -140,37 +164,60 @@ const confirmDelete = (event) => {
   });
 };
 
+function allInputChanged(){
+  nameChanged();
+  keyChanged();
+  urlChanged()
+}
+
 function nameChanged() {
   if (userName.value === "") {
     createErrorToast("Input Error", "Name cannot be empty")
+    nameNotChanged.value = true
+  } else {
+    nameNotChanged.value = savedName.value === userName.value;
   }
+  changeSaveButton()
 }
 
 function keyChanged() {
   if (password.value === "") {
     createErrorToast("Input Error", "Admin Api Key cannot be empty")
+    passwordNotChanged.value = true
+  } else {
+    passwordNotChanged.value = savedPassword.value === password.value;
   }
+  changeSaveButton()
 }
 
 function urlChanged() {
   if (url.value === "") {
     createErrorToast("Input Error", "Url cannot be empty")
+    urlNotChanged.value = true
+  } else {
+    urlNotChanged.value = savedUrl.value === url.value;
   }
+  changeSaveButton()
 }
 
 watch(selectedCredentials, async (newVal) => {
+  saveDisabled.value = true;
   if (newVal) {
     await insertCredentials(newVal.name).then(() => {
-      checkConnection()
+      checkConnection();
     });
   }
 })
+
+function changeSaveButton() {
+  saveDisabled.value = nameNotChanged.value === true && urlNotChanged.value === true && passwordNotChanged.value === true;
+}
 
 onMounted(() => {
   loadLastSaved();
   loadCredentialList();
   checkConnection();
-  setInterval(checkConnection, 1000 * 400);
+  setInterval(checkConnection, 1000 * 30);
 });
 
 </script>
@@ -193,7 +240,7 @@ onMounted(() => {
 
     <div class="ml-auto p-1">
       <Button v-tooltip.left="'Config'" icon="pi pi-cog" @click="visible = true"/>
-      <span v-if="url === ''|| password === '' " class="pi pi-exclamation-triangle text-3xl text-yellow-500 ml-2"
+      <span v-if="url === ''|| password === '' " class="pi pi-exclamation-triangle text-3xl text-yellow-500 ml-2 mb-2"
             v-tooltip.left="'Missing Credentials'"></span>
     </div>
 
@@ -232,9 +279,10 @@ onMounted(() => {
                   placeholder="Select Option"
                   class="w-full md:w-14rem"/>
       </div>
-      <Button icon="pi pi-save" class="ml-auto" @click="saveCredentials" v-tooltip.bottom="'Save Credentials'"/>
+      <Button icon="pi pi-save" class="ml-auto" @click="saveCredentials" v-tooltip.bottom="'Save Credentials'"
+              :disabled="saveDisabled"/>
       <Button icon="pi pi-trash" class="ml-auto" @click="confirmDelete($event)" v-tooltip.bottom="'Delete Credentials'"
-              :disabled="disabled"/>
+              :disabled="deleteDisabled"/>
     </div>
   </Dialog>
 
