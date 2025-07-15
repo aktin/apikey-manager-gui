04aed85aa8010b638b28bd27ac39ec0110373ad4
akomii <alexkombie@gmail.com>
2025-07-15 11:22:15 +0200

    updated app metadata, added linting

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+// eslint.config.js
+import vue from 'eslint-plugin-vue';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import parser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['**/*.ts', '**/*.vue'],
+    languageOptions: {
+      parser: parser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      vue,
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...vue.configs['vue3-essential'].rules,
+      ...tseslint.configs.recommended.rules,
+    },
+  },
+];
