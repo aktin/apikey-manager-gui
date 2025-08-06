00a19a1d8bece4f6ab4b0b12b8b4955fa297ef53
akomii <alexkombie@gmail.com>
2025-08-06 15:10:43 +0200

    rearranged components in App.vue, fixed bug in ToastWrapper.ts

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import {ref} from "vue";
-import NodeCredentialsForm from "./components/NodeCredentialsForm.vue";
-import NodeCredentialsTable from "./components/NodeCredentialsTable.vue";
+import NodeCredsForm from "./components/NodeCredsForm.vue";
+import NodeCredsTable from "./components/NodeCredsTable.vue";
 import BrokerProfileManager from "./components/BrokerProfileManager.vue";
 import BrokerStatusIndicator from "./components/BrokerStatusIndicator.vue";
 import Toast from "primevue/toast";
@@ -13,11 +13,13 @@ const selectedApiKey = ref("");
   <Toast/>
   <div class="m-3">
     <div class="grid">
-      <NodeCredentialsTable @update:selectedApiKey="selectedApiKey = $event" class="col-10 surface-200 border-round-md"/>
-      <div class="col-2">
-        <BrokerStatusIndicator/>
-        <BrokerProfileManager/>
-        <NodeCredentialsForm :selectedKey="selectedApiKey" class="surface-200 border-round-md"/>
+      <NodeCredsTable @update:selectedApiKey="selectedApiKey = $event" class="col-10 surface-200 border-round-md"/>
+      <div class="col-2 flex flex-column gap-2">
+        <div class="flex align-items-center justify-content-between">
+          <BrokerStatusIndicator/>
+          <BrokerProfileManager/>
+        </div>
+        <NodeCredsForm :selectedKey="selectedApiKey" class="surface-200 border-round-md"/>
       </div>
     </div>
   </div>
--- src/utils/ToastWrapper.ts
+++ src/utils/ToastWrapper.ts
@@ -5,7 +5,9 @@
  * Requires `useToast()` to be passed from the caller for instance safety.
  */
 
-import type {ToastServiceMethods} from 'primevue/usetoast';
+import { useToast } from "primevue/usetoast";
+
+type ToastType = ReturnType<typeof useToast>;
 
 const toastLife = 5000;
 
@@ -14,7 +16,7 @@ const toastLife = 5000;
  * @param title - The error title (e.g. "Error", "Access Denied")
  * @param detail - The error detail message
  */
-export function createErrorToast(toast: ToastServiceMethods, title: string, detail: string): void {
+export function createErrorToast(toast: ToastType, title: string, detail: string): void {
   toast.add({
     severity: 'error',
     summary: title,
@@ -28,7 +30,7 @@ export function createErrorToast(toast: ToastServiceMethods, title: string, deta
  * @param title - The success title (e.g. "Success", "Access Granted")
  * @param detail - The success detail message
  */
-export function createSuccessToast(toast: ToastServiceMethods, title: string, detail: string): void {
+export function createSuccessToast(toast: ToastType, title: string, detail: string): void {
   toast.add({
     severity: 'success',
     summary: title,
