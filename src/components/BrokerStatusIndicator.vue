37ff285af05247a5354414ba8d7f6e710a4dc923
akomii <alexkombie@gmail.com>
2025-08-06 15:26:35 +0200

    fixed bug for wrong indicator on creds delete

--- src/components/BrokerStatusIndicator.vue
+++ src/components/BrokerStatusIndicator.vue
@@ -4,9 +4,16 @@ import BrokerConnection from "../services/BrokerConnection";
 import {useI18n} from "vue-i18n";
 
 const {t} = useI18n();
-const connected = ref<boolean>(false);
+
+const connected = ref(false);
+const creds = ref(BrokerConnection.getCredentials());
 
 async function checkConnection(): Promise<void> {
+  creds.value = BrokerConnection.getCredentials();
+  if (!creds.value.url || !creds.value.adminApiKey) {
+    connected.value = false;
+    return;
+  }
   connected.value = await BrokerConnection.isConnected();
 }
 
@@ -21,21 +28,21 @@ onMounted(() => {
 <template>
   <div class="flex align-items-center">
     <!-- Green or red indicator based on connection -->
-    <div v-if="connected" class="flex align-items-center text-green-600 text-xl" v-tooltip.left="BrokerConnection.getCredentials().url">
+    <div v-if="connected" class="flex align-items-center text-green-600 text-xl" v-tooltip.left="creds.url">
       <i class="pi pi-circle-fill mx-2"/>
       <p class="font-bold">{{ t("indicator.connected") }}</p>
     </div>
 
-    <div v-else class="flex align-items-center text-red-600 text-xl" v-tooltip.left="BrokerConnection.getCredentials().url">
+    <div v-else class="flex align-items-center text-red-600 text-xl" v-tooltip.left="creds.url">
       <i class="pi pi-circle-fill mx-2"/>
       <p class="font-bold">{{ t("indicator.noConnection") }}</p>
     </div>
 
     <!-- Warnings for missing credentials -->
     <div class="ml-auto p-1">
-    <span v-if="BrokerConnection.areCredentialsInitialized() && (BrokerConnection.getCredentials().url === '' || BrokerConnection.getCredentials().adminApiKey === '')"
-          class="pi pi-exclamation-triangle text-3xl text-yellow-500 ml-2 mb-2"
-          v-tooltip.left="t('indicator.missingCredentials')"/>
+      <span v-if="BrokerConnection.areCredentialsInitialized() && (!creds.url || !creds.adminApiKey)"
+            class="pi pi-exclamation-triangle text-3xl text-yellow-500 ml-2 mb-2"
+            v-tooltip.left="t('indicator.missingCredentials')"/>
     </div>
   </div>
 </template>
