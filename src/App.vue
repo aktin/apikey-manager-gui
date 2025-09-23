456185f8d0a79c0c2999c7f172418e3866c9093a
akomii <alexkombie@gmail.com>
2025-09-23 16:22:02 +0200

    added Tabs to main view

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,8 @@ import NodeCredsForm from "./components/NodeCredsForm.vue";
 import NodeCredsTable from "./components/NodeCredsTable.vue";
 import BrokerProfileManager from "./components/BrokerProfileManager.vue";
 import BrokerStatusIndicator from "./components/BrokerStatusIndicator.vue";
+import TabView from 'primevue/tabview';
+import TabPanel from 'primevue/tabpanel';
 import Toast from "primevue/toast";
 
 const selectedApiKey = ref("");
@@ -23,19 +25,25 @@ const selectedApiKey = ref("");
 
 <template>
   <Toast/>
-  <div class="m-3">
-    <div class="grid">
-      <NodeCredsTable
-          @update:selectedApiKey="selectedApiKey = $event"
-          class="col-10 surface-200 border-round-md"
-      />
-      <div class="col-2 flex flex-column gap-2">
-        <div class="flex align-items-center justify-content-between">
-          <BrokerStatusIndicator/>
-          <BrokerProfileManager/>
-        </div>
-        <NodeCredsForm :selectedKey="selectedApiKey" class="surface-200 border-round-md"/>
-      </div>
+  <header class="surface-100 border-round-md">
+    <div class="flex flex-wrap align-items-center justify-content-end gap-3 mr-3">
+      <BrokerStatusIndicator/>
+      <BrokerProfileManager/>
     </div>
+  </header>
+  <div class="m-3">
+    <TabView class="border-solid">
+      <TabPanel header="API Keys">
+        <div class="grid">
+          <NodeCredsTable
+              @update:selectedApiKey="selectedApiKey = $event"
+              class="col-10 surface-200 border-round-md"
+          />
+          <div class="col-2">
+            <NodeCredsForm :selectedKey="selectedApiKey" class="surface-200 border-round-md"/>
+          </div>
+        </div>
+      </TabPanel>
+    </TabView>
   </div>
 </template>
