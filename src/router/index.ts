835a3977986af5e45f2eed80a8a967300ad0552c
akomii <alexkombie@gmail.com>
2025-10-21 16:34:57 +0200

    hotfix: prevent initial 404 by switching to hash history in Electron build

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import {createRouter, createWebHashHistory} from 'vue-router'
 import Error404 from "../views/Error404.vue";
 import Error500 from "../views/Error500.vue";
 import ApiKeyView from "../views/ApiKeyView.vue";
@@ -22,7 +22,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
